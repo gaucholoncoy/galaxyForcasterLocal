eea9d06c308e83c2d3a2f8264caf280b9f5c3825
rrl <rlanus@gmail.com>
2018-08-30 20:35:09 -0300

    Seteo Entorno

---
+++ src/com/galaxyForcaster/Forcaster.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.galaxyForcaster;
+
+/**
+ * @author richard
+ *
+ */
+public class Forcaster {
+
+	/**
+	 * 
+	 */
+	public Forcaster() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
